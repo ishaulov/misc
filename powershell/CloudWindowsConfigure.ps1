a291749d6da8aaffeaa41c29baacf2e97fe44eea
Ilya Shaulov <41745869+ishaulov@users.noreply.github.com>
2025-02-06 14:39:33 +0500

    изменил ранлевел

--- powershell/CloudWindowsConfigure.ps1
+++ powershell/CloudWindowsConfigure.ps1
@@ -1,7 +1,7 @@
 New-NetFirewallRule -DisplayName "Allow ICMPv4-In" -Direction Inbound -Protocol ICMPv4 -Action Allow
 
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Command Start-Sleep -Seconds 60; Get-NetAdapter | Restart-NetAdapter -Confirm"
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Command Start-Sleep -Seconds 60; Get-NetAdapter | Restart-NetAdapter"
 $trigger = New-ScheduledTaskTrigger -AtStartup
 $settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
 Register-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Principal $principal -TaskName "RestartNetAdapters" -Description "Restart Net Adapter"
