183c01c1b74687844c3862b4f22dd429d75b9fc4
Ilya Shaulov <41745869+ishaulov@users.noreply.github.com>
2025-02-07 10:09:44 +0500

    добавил комментарии для CloudWinConfig

--- powershell/CloudWindowsConfigure.ps1
+++ powershell/CloudWindowsConfigure.ps1
@@ -1,4 +1,12 @@
+# Время на инициализацию после запуска
 Start-Sleep -Seconds 30
-"Hello! Date: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" | Out-File -FilePath "C:\Users\Administrator\Documents\date.txt"
+
+# Установка времени Екб (GMT +5) и запись даты в файл
+Set-TimeZone -Id "Ekaterinburg Standard Time"
+"Hello! Date: $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')" | Out-File -FilePath "C:\Users\$($env:USERNAME)\Documents\date.txt"
+
+# Разрешение ICMPv4
 New-NetFirewallRule -DisplayName "Allow ICMPv4-In" -Direction Inbound -Protocol ICMPv4 -Action Allow
+
+# Перезапуск ПК
 Restart-Computer -Force
